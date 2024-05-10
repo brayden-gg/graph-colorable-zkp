0d05daac8ab642abf5aaeaea34c152188a8a2c8b
brayden-gg <bgoldsteingelb@gmail.com>
2024-05-10 02:03:47 -0400

    woo

--- src/example_graphs.py
+++ src/example_graphs.py
@@ -12,7 +12,7 @@ petersen.add_edges_from([(0, 5), (1, 6), (2, 7), (3, 8), (4, 9),
                          (5, 6), (6, 7), (7, 8), (8, 9), (9, 5),
                          (0, 2), (2, 4), (4, 1), (1, 3), (3, 0)])
 petersen_col = [0, 1, 1, 2, 2, 1, 0, 2, 1, 0]
-petersen_col_cheat = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+petersen_col_cheat = [0, 1, 1, 2, 0, 1, 1, 2, 2, 2]
 
 graphs = {"k3": k3, "petersen": petersen}
 colorings = {"k3": {"True": k3_col, "False": k3_col_cheat}, "petersen": {"True": petersen_col, "False": petersen_col_cheat}}
